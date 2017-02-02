f7438cd876518f55fbb12cb67242352b8f8c3bb9
Chris Sharkey <ev2900@gmail.com>
2017-02-02 15:04:43 -0500

    working on grouping logic

--- routes/get.js
+++ routes/get.js
@@ -14,31 +14,46 @@ router.route('/get')
 .get(function(req, res) {
 	data.find(function(err, data) {
 		
-		// error 
+		// Error 
 		if (err) 
 			res.send(err);
 		
-		// sort the data
+		// Sort the Data
 		var sortedByOrder = data.sort(function(a,b) {
 			return a.order - b.order;
 		});
 
-		// grouping
+		// Grouping Logic
 		var output = [];
-		for(var i=0; i<sortedByOrder.length; i++) {
-			if(sortedByOrder[i].type === 'Person') {
+
+			// handling base case
+			if(sortedByOrder[0].type === 'Person'){
 				output.push({
 					"type": sortedByOrder[i].type,
 					"order": sortedByOrder[i].order,
 					"people": [sortedByOrder[i].name]
-				});
+				})
 			}
-		}
-		res.send(output);	
+			else if(sortedByOrder[0].type === 'Place'){
+				output.push{
+					"type": sortedByOrder[i].type,
+					"order": sortedByOrder[i].order,
+					"name": sortedByOrder[i].name
+				}
+			}
 
+			// handling the remander of the list 
+			for(var i=1; i<sortedByOrder.length; i++) {
+				if(sortedByOrder[i].type === 'Person') {
+					if(output[output.length-1].type === sortedByOrder[i])
+
+					output[output.length-1].people.push(sortedByOrder[i].name);
+				}
+			}
+		
+		res.send(output);	
 	});
 });
-
 
 // Return router
 module.exports = router;
