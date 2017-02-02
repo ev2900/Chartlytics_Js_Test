42d006b37cc2d3e1976e678fca56aa95825cf1b4
Chris Sharkey <ev2900@gmail.com>
2017-02-02 08:24:03 -0500

    working on renaming api route

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 //Models
-var Spot = require('../models/spot');
+var Spot = require('../models/data');
 
 // Routes
 Spot.methods(['get', 'put', 'post', 'delete']);
-Spot.register(router, '/spots');
+Spot.register(router, '/upload');
 
 // Return router
 module.exports = router;
