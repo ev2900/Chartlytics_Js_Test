e3955a53bda9bc4a932da0ab33f92c5811d1eb2c
Chris Sharkey <ev2900@gmail.com>
2017-02-03 14:05:38 -0500

    reposition if statment

--- public/js/ajax/output.js
+++ public/js/ajax/output.js
@@ -6,14 +6,15 @@ $(function () {
 		url: 'http://35.161.233.165:9000/api/get',
 		success: function(output) {
 			$.each(output, function(i, item) {
-				
-				if(item.name == "undefined") {
-					alert("undefined");
-				}
-
 				$output.append(
+					
 					'<p>' + item.type + '</p>'		+
 					'<p>' + item.order + '</p>'		
+				
+					if(item.name == "undefined") {
+						alert("undefined");
+					}
+
 				);
 
 				alert('test');
