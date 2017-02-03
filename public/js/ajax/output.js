fae3f96ff6d29f3ebda6fe28006afea1c6d84ffb
Chris Sharkey <ev2900@gmail.com>
2017-02-03 13:22:27 -0500

    working out ajax

--- public/js/ajax/output.js
+++ public/js/ajax/output.js
@@ -5,11 +5,7 @@ $(function () {
 		type: 'GET',
 		url: 'http://35.161.233.165:9000/api/get',
 		success: function(output) {
-			$.each(output, function(i, item) {
-				output.append(
-					item.type
-				);
-			});
+			
 		}
 	});
 });
