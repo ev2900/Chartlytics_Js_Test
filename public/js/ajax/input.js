1bce2301f202fd9b8f7f041d9f02909b9b386489
Chris Sharkey <ev2900@gmail.com>
2017-02-03 15:43:17 -0500

    more style changes

--- public/js/ajax/input.js
+++ public/js/ajax/input.js
@@ -1,31 +1,4 @@
 $(function () {
 
-	var $output = $('#output');
-	$.ajax({
-		type: 'GET',
-		url: 'http://35.161.233.165:9000/api/get',
-		success: function(output) {
-			$.each(output, function(i, item) {
-				
-				if(item.name) {
-					$output.append(
-						'<p>' + 'Type: ' + item.type + '</p>'	+
-						'<p>' + 'Order: ' + item.order + '</p>' 	+
-						'<p>' + 'Name: ' + item.name + '</p>'	+
-						'<br>'	
-					);
-				}
-
-				else if(item.people) {
-					$output.append(
-						'<p>' + 'Type: ' + item.type + '</p>'	+
-						'<p>' + 'Order: ' + item.order + '</p>' 	+
-						'<p>' + 'Name: ' + item.people + '</p>'	+
-						'<br>'	
-					);	
-				}
-
-			});		
-		}
-	});
+	
 });
