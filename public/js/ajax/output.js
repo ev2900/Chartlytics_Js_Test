f71ac55d95714c08a39e72ea1439ba9f79008f91
Chris Sharkey <ev2900@gmail.com>
2017-02-03 14:03:17 -0500

    reworking at the if statment

--- public/js/ajax/output.js
+++ public/js/ajax/output.js
@@ -6,12 +6,15 @@ $(function () {
 		url: 'http://35.161.233.165:9000/api/get',
 		success: function(output) {
 			$.each(output, function(i, item) {
+				
+				if(item.name == "undefined") {
+					alert("undefined");
+				}
 
 				$output.append(
 					'<p>' + item.type + '</p>'		+
 					'<p>' + item.order + '</p>'		
 				);
-
 			});		
 		}
 	});
