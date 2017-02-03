14eda407aa943110f5e32672d27576078cd03542
Chris Sharkey <ev2900@gmail.com>
2017-02-03 15:08:50 -0500

    Update on the ajax

--- public/js/ajax/output.js
+++ public/js/ajax/output.js
@@ -6,19 +6,17 @@ $(function () {
 		url: 'http://35.161.233.165:9000/api/get',
 		success: function(output) {
 			$.each(output, function(i, item) {
+				
+				if(item.test) {
+					alert("hey");
+				}
+
 				$output.append(
-					
 					'<p>' + item.type + '</p>'		+
 					'<p>' + item.order + '</p>'		
-				
-					if(item.name == "undefined") {
-						alert("undefined");
-					}
-
 				);
-
-				alert('test');
-			});		
+			});	
+			alert('loop ran');	
 		}
 	});
 });
