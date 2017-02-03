97a7dc116eb40dd14218cf649be99a4ecb810f5d
Chris Sharkey <ev2900@gmail.com>
2017-02-03 13:55:20 -0500

    debuggin the ajax call

--- public/js/ajax/output.js
+++ public/js/ajax/output.js
@@ -6,13 +6,16 @@ $(function () {
 		url: 'http://35.161.233.165:9000/api/get',
 		success: function(output) {
 			$.each(output, function(i, item) {
-				$output.append(
-					'<p>' + item.type + '</p>'		+
-					'<p>' + item.order + '</p>'		+
-				)
+				
 				if(item.name == "undefined") {
 					var $a = 'a';
 				}
+
+				$output.append(
+					'<p>' + item.type + '</p>'		+
+					'<p>' + item.order + '</p>'		+
+				);
+
 			});		
 		}
 	});
