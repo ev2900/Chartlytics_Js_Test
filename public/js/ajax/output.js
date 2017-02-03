338b5e16791294ab4f0ca7f54af654996daad62a
Chris Sharkey <ev2900@gmail.com>
2017-02-03 14:00:46 -0500

    Problem was extra + ?

--- public/js/ajax/output.js
+++ public/js/ajax/output.js
@@ -6,10 +6,10 @@ $(function () {
 		url: 'http://35.161.233.165:9000/api/get',
 		success: function(output) {
 			$.each(output, function(i, item) {
-				
+
 				$output.append(
 					'<p>' + item.type + '</p>'		+
-					'<p>' + item.order + '</p>'		+
+					'<p>' + item.order + '</p>'		
 				);
 
 			});		
