2ada182a1c74c0d6d6286701e4d4e0c6f0f46203
Chris Sharkey <ev2900@gmail.com>
2017-02-03 13:27:17 -0500

    working on ajax call

--- public/js/ajax/output.js
+++ public/js/ajax/output.js
@@ -6,7 +6,9 @@ $(function () {
 		url: 'http://35.161.233.165:9000/api/get',
 		success: function(output) {
 			$.each(output, function(i, item) {
-				
+				output.append(
+					'<p>' + item.type + '<p>'
+				);
 			});		
 		}
 	});
