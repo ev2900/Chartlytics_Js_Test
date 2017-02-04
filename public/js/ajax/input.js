4f2ec685d57e0ca223b4c08d50cee45c7f1e9704
Chris Sharkey <ev2900@gmail.com>
2017-02-04 02:21:35 -0500

    added post functionality for place

--- public/js/ajax/input.js
+++ public/js/ajax/input.js
@@ -19,10 +19,10 @@ $(function () {
 			url: 'http://35.161.233.165:9000/api/upload',
 			data: new_person,
 			success: function() {
-				alert('good');
+				 window.location.replace("/");
 			},
 			error: function() {
-				alert('bad');
+				alert('error');
 			}
 		});
 
@@ -32,5 +32,33 @@ $(function () {
 
 // Place
 $(function () {
+
+	var $place_person = $('#place_Person'); 
+	var $place_order = $('#place_order');
+	var $place_name = $('#place_place');
+
+	$('#place').on('submit', function(ev) {
+		ev.preventDefault();
+
+		var new_place = {
+			type:$place_person.val(),
+			order:$place_order.val(),
+			name:$place_name.val()
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: 'http://35.161.233.165:9000/api/upload',
+			data: new_place,
+			success: function() {
+				window.location.replace("/");
+			},
+			error: function() {
+				alert('error');
+			}
+
+		});
+
+	});
 
 });
